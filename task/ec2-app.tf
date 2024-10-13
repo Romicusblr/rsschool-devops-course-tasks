f810935373f00385f7374abbf4b2cada169ce543
Raman Mastyka <raman.mastyka@gmail.com>
2024-10-13 12:32:30 +0200

    fix: update instances

--- task/ec2-app.tf
+++ task/ec2-app.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "app" {
   ami           = data.aws_ami.ubuntu_22.id
-  instance_type = "t2.micro"
+  instance_type = "t3.nano"
   key_name      = aws_key_pair.key.key_name
+  private_ip    = cidrhost(aws_subnet.private[0].cidr_block, 10)
   subnet_id     = element(aws_subnet.private[*].id, 0)
 
   vpc_security_group_ids = [aws_security_group.app_sg.id]
--- task/ec2-bastion.tf
+++ task/ec2-bastion.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "bastion" {
-  ami           = data.aws_ami.ubuntu_22.id
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.key.key_name
-  subnet_id     = element(aws_subnet.public[*].id, 0)
-
+  ami                    = data.aws_ami.ubuntu_22.id
+  instance_type          = "t3.nano"
+  key_name               = aws_key_pair.key.key_name
+  private_ip             = cidrhost(aws_subnet.public[0].cidr_block, 10)
+  subnet_id              = element(aws_subnet.public[*].id, 0)
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
   tags = merge(local.common_tags, {
