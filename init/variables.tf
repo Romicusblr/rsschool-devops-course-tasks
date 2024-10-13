6fdb2bf21d24b82a8339fa5dd1551e5a29d44cfe
Raman Mastyka <raman.mastyka@gmail.com>
2024-10-13 10:49:43 +0200

    fix: update variables resolving

--- init/variables.tf
+++ init/variables.tf
@@ -1,18 +1,3 @@
-variable "account_id" {
-  type        = string
-  description = "AWS Account ID"
-}
-
-variable "region" {
-  type        = string
-  description = "AWS Region"
-}
-
-variable "bucket_name" {
-  type        = string
-  description = "State bucket name"
-}
-
 variable "github_repository" {
   type        = string
   description = "Github repository name"
--- task/variables.tf
+++ task/variables.tf
@@ -1,23 +1,3 @@
-variable "account_id" {
-  type        = string
-  description = "AWS Account ID"
-}
-
-variable "region" {
-  type        = string
-  description = "AWS Region"
-}
-
-variable "bucket_name" {
-  type        = string
-  description = "State bucket name"
-}
-
-variable "github_repository" {
-  type        = string
-  description = "Github repository name"
-}
-
 variable "vpc_ip_block" {
   type        = string
   description = "VPC IP block"
